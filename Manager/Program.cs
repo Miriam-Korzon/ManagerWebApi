98b1ecb434e164659674bd84277261af4039c81e
Miriam Korzon <37325953446@mby.co.il>
2024-06-04 17:48:47 +0300

    Add project files.

---
+++ Manager/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "Data Source=SRV2\\PUPILS;Initial Catalog=325953446_Products;Integrated Security=True";
+
+        DataAccess da = new DataAccess();
+
+        da.insertCategory(connectionString);
+
+
+    }
+}
+
+
